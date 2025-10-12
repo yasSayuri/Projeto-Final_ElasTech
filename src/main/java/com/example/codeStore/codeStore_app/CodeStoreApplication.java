b16bb43de77a5c6908fd7a5f689ed8cab5edd0d9
Yasmin Sayuri <yascar@alunos.utfpr.edu.br>
2025-10-12 15:26:03 -0300

    Primeiro commit com o projeto

---
+++ src/main/java/com/example/codeStore/codeStore_app/CodeStoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.codeStore.codeStore_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/codeStore/codeStore_app/CodeStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.codeStore.codeStore_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
