0b3f5b27dd3474c51af3b96c902b6fc0fdfb3966
Isabella Teixeira <bella.silva015@gmail.com>
2025-10-13 23:10:01 -0300

    add service,repository,controller p/ pedido

--- src/main/java/com/example/codeStore/codeStore_app/service/PedidoService.java
+++ src/main/java/com/example/codeStore/codeStore_app/service/PedidoService.java
@@ -1,5 +1,12 @@
 package com.example.codeStore.codeStore_app.service;
 
-public class PedidoService {
+import java.util.List;
 
+import com.example.codeStore.codeStore_app.model.PedidoModel;
+
+public interface PedidoService {
+    List<PedidoModel> obterTodos();
+    PedidoModel obterPorId(Long id);
+    PedidoModel cadastrarPedido(PedidoModel novo);
+    PedidoModel atualizarPedido(Long id, PedidoModel atualizacao);
 }
